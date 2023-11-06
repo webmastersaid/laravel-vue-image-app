d66eb76866f8c6967d422cd9f17e28ade311fecb
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-06 13:37:00 +0500

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.index')
+@section('content')
+    <div id="app"></div>
+@endsection
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
