127d5b3b1c2bde96dbfb357921727b7639a486ac
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-06 14:00:06 +0500

    image model

---
+++ database/migrations/2023_11_06_084835_create_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('url');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+            $table->index('post_id', 'image_post_idx');
+            $table->foreign('post_id', 'image_post_fk')->on('posts')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
