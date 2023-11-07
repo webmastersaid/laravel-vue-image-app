9c275f8b4b01372fbe945829f13f4bde9ea5c31f
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-07 13:43:13 +0500

    posts

---
+++ app/Http/Controllers/Post/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $posts = Post::all();
+        return PostResource::collection($posts);
+    }
+}
